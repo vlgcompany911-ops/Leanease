b25d3b7699c126c43b37c359d70f11467119ab15
vlgcompany911-ops <vlgcompany911@gmail.com>
2026-03-24 22:58:31 +0100

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '.')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('LearnEase Server Running');
+});
